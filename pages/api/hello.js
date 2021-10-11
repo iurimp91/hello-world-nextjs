4cde6efc6f2ee3186eef03520c67b15f6faa97b9
Iuri Magnago <iurimp91@gmail.com>
2021-10-11 19:55:48 -0300

    feat: print hello world on screen when button is pressed

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    return res.status(200).send({ hello: "world" });
+  } catch (e) {
+    return res.status(500);
+  }
 }
